6e7d974e0169646931840156289727ed5cdea287
Caren-Koroeny <carensiya5@gmail.com>
2022-04-14 01:02:07 -0700

    print number less than 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for(n = 15; n < 25; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}

1bfae0fad3d4284ed73664321b5b704d441bca83
Caren-Koroeny <carensiya5@gmail.com>
2022-04-14 00:40:13 -0700

    Updated 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include"main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @letter: the character to be tracked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int _isupper(int alphabet)
+{
+	return(alphabet >= 'A' && alphabet <= 'Z');
+}

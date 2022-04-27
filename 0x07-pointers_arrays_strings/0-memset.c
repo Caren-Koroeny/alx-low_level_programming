9a2d5fb17f561eefbe6ce70775b89d955b911607
Caren-Koroeny <carensiya5@gmail.com>
2022-04-26 18:14:45 -0700

    updated memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	char *start = s;
 
-	while (i < n)
+	while (n--)
 	{
-		s[i] = b;
-		i++;
+		*s = b;
+		s++;
 	}
-	return (s);
+	return (start);
 }

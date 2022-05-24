5846a495f3f3bef5d309b5918c322f431747c1d7
Caren-Koroeny <carensiya5@gmail.com>
2022-05-24 06:53:48 -0700

    chagnges

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t caren;
 
-	nelem = 0;
+	caren = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		caren++;
 	}
-	return (nelem);
+	return (caren);
 }
